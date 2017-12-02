af0c28760215a71bde158f5c6bb57ce494a9000b
Alex Kafer <kafer005@umn.edu>
2017-12-02 13:34:32 -0600

    Added third color and pong game features!

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'production': {
+      'accessKeyId': 'AKIAI6D3EB3TJHRJCUYQ',
+      'secretAccessKey': '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',
+      'region': 'us-east-2',
+      'params': {
+          'Bucket': 'lightshow-web'
+      }
+    },
+    'productionHeaders': {},
+  };

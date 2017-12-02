5312f619685e668fc5733e96eb9064361af0dd26
Alex Kafer <kafer005@umn.edu>
2017-12-02 13:38:06 -0600

    Point to the public EC2 server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,11 +23,11 @@ export class AppComponent implements OnInit {
   public placeInLine = 0;
   public totalInLine = 0;
 
-  private ws = new $WebSocket("ws://10.128.111.165:8080");
+  private ws = new $WebSocket("ws://54.89.142.238:8080");
 
   ngOnInit(): void {
      // Make the HTTP request:
-     this.http.get('http://10.128.111.165:8000').subscribe((data:any) => {
+     this.http.get('http://54.89.142.238:8000').subscribe((data:any) => {
       // Read the result field from the JSON response.
       this.pixels = data.pixels;
       this.mode = data.mode;
